d1cc679fb0af13832972655311c326d1dde9f6a8
kaylee-sys <jannahapetyan32@gmail.com>
2026-08-20 22:26:31 +0500

    Delete src directory

--- src/app/actions.ts
+++
@@ -1,6 +0,0 @@
-"use server"
-
-export async function triggerServerError() {
-  // This error will appear in Node.js terminal
-  throw new Error("Server runtime error triggered from clicker!")
-}
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AppShell } from "@/components/AppShell";
-
-export default function IndexPage() {
-  return <AppShell />;
-}
